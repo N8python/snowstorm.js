970e38de35c4e5469e99ffe299b0880915c098fe
n8programs <43304488+N8python@users.noreply.github.com>
2018-12-27 17:05:43 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+$(init)
+function randInt(min, max) {
+  return min + Math.floor(Math.random() * (max - min))
+}
+var frame = 0;
+class Snowflake {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  draw() {
+    $("body").append(`<svg style="position: absolute; top: ${this.y}px; left: ${this.x}px;" >
+      <circle cx="5" cy="5" r="5" fill="white"/>
+      </svg>`)
+    this.y += 3;
+  }
+}
+snowflakes = []
+
+function spawnSnowflakes(){
+  if (frame % 25 === 0){
+    snowflakes.push(new Snowflake(randInt(0, window.innerWidth-50), 0))
+  }
+}
+function init() {
+  $(function() {
+    $('.same').matchHeight();
+  });
+  var snowstorm = setInterval(() => {
+    spawnSnowflakes();
+    $('svg').remove();
+    snowflakes.forEach(snowflake => {
+      snowflake.draw();
+    })
+    for(var i = 0; i < snowflakes.length; i++){
+      if(snowflakes[i].y>$("body").height()){
+        snowflakes.splice(i, 1)
+      }
+    }
+    frame+=1;
+    console.log(frame);
+  }, 5)
+}
